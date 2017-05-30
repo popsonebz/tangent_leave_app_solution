e01714b6b3cee5c16ed0e7a9d054058096508bd4
popsonebz <popoolaebenezer@gmail.com>
2017-05-30 09:33:11 +0200

    added management command

--- leave/management/commands/carry_leave_over_the_year.py
+++ leave/management/commands/carry_leave_over_the_year.py
@@ -6,4 +6,9 @@ class Command(BaseCommand):
 	help = "This event would be run daily by crontab for leave to be carried over the next year if the current day is the first day of the year"
 
 	def handle(self, *args, **options):
-		self.stdout.write("Doing All The Things!")
+		#self.stdout.write("Doing All The Things!")
+		employees = employee.objects.all()
+		for emp in employees:
+			if emp.number_of_leave_days_left > 5:
+			    emp.number_of_leave_days_left = 5
+			    emp.save()
