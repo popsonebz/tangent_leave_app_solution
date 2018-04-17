73cd8cb9442627c54a1a63ed0b6635befe61f28f
Popoola Ebenezer <popoolaebenezer@gmail.com>
2018-04-17 18:15:50 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python manage.py migrate                  # Apply database migrations
+python manage.py collectstatic --noinput  # Collect static files
+
+# Prepare log files and start outputting logs to stdout
+touch gunicorn.log
+touch access.log
+tail -n 0 -f *.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn exporter.wsgi:application \
+    --name exporter \
+    --bind 0.0.0.0:1234 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=gunicorn.log \
+    --access-logfile=access.log \
+    "$@"
