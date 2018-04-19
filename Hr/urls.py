280a5c739a5eb25cafc9ac80cd68a70ed3c9005e
Popoola Ebenezer <popoolaebenezer@gmail.com>
2018-04-19 12:35:20 +0200

    Update urls.py

--- Hr/urls.py
+++ Hr/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
 	      url(r'^leave/', include('leave.urls', namespace='leave')),
