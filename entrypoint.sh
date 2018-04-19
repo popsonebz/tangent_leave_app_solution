95fc2f5a8d0c8713adfdea014db00d557346cccc
Popoola Ebenezer <popoolaebenezer@gmail.com>
2018-04-19 11:41:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #python manage.py migrate                  # Apply database migrations
-#python manage.py collectstatic --noinput  # Collect static files
+python manage.py collectstatic --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
 touch gunicorn.log
